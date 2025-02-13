246c20483d2726002d8e0cada7090cfe3aa134d5
lyucheng <lyucheng@umich.edu>
2025-02-13 02:01:59 -0500

    update read functions

--- scripts/1_compile_variants_by_gene.py
+++ scripts/1_compile_variants_by_gene.py
@@ -5,29 +5,29 @@
 import os
 from Bio import SeqIO
 
-# Read function that reads gff file for the reference genome
-def read_gff(gff_file):
+# Read function that reads gff3 file for the reference genome
+def read_gff3(gff3_file):
     gene_dict = {}
-    with open(gff_file, 'r') as gff:
-        for line in gff:
+    with open(gff3_file, 'r') as gff3:
+        for line in gff3:
             if not line.startswith('#'):
                 line = line.strip().split('\t')
                 if line[2] == 'gene':
-                    gene_id = line[-1].split(';')[0].split('=')[1]
+                    gene_id = line[8].split(';')[0].split('=')[1]
                     gene_dict[gene_id] = {'start': int(line[3]), 'end': int(line[4])}
     return gene_dict
 
-# Read function that reads the variant file and compile the variants by gene
-def read_variants(variant_file, gene_dict):
+# Read function that reads the vcf and compile the variants by gene
+def read_vcf(vcf_file, gene_dict):
     gene_variants = {}
-    with open(variant_file, 'r') as variants:
-        for line in variants:
+    with open(vcf_file, 'r') as vcf:
+        for line in vcf:
             if not line.startswith('#'):
                 line = line.strip().split('\t')
                 gene_id = line[0]
                 pos = int(line[1])
-                ref = line[2]
-                alt = line[3]
+                ref = line[3]
+                alt = line[4]
                 if gene_id in gene_dict:
                     if gene_id not in gene_variants:
                         gene_variants[gene_id] = []
