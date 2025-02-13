a25b7fdc1b7a54d7dd6be4c38da9b26891b05ae3
lyucheng <lyucheng@umich.edu>
2025-02-13 01:42:26 -0500

    Add read functions to 1_compile_variatns_by_genes.py

--- scripts/1_compile_variants_by_gene.py
+++ scripts/1_compile_variants_by_gene.py
@@ -1,1 +1,36 @@
+# This is a script that compiles the variants by gene from the variant files
+# Author: Yu-Cheng Lin
+# Time: 2025-02-12
 
+import os
+from Bio import SeqIO
+
+# Read function that reads gff file for the reference genome
+def read_gff(gff_file):
+    gene_dict = {}
+    with open(gff_file, 'r') as gff:
+        for line in gff:
+            if not line.startswith('#'):
+                line = line.strip().split('\t')
+                if line[2] == 'gene':
+                    gene_id = line[-1].split(';')[0].split('=')[1]
+                    gene_dict[gene_id] = {'start': int(line[3]), 'end': int(line[4])}
+    return gene_dict
+
+# Read function that reads the variant file and compile the variants by gene
+def read_variants(variant_file, gene_dict):
+    gene_variants = {}
+    with open(variant_file, 'r') as variants:
+        for line in variants:
+            if not line.startswith('#'):
+                line = line.strip().split('\t')
+                gene_id = line[0]
+                pos = int(line[1])
+                ref = line[2]
+                alt = line[3]
+                if gene_id in gene_dict:
+                    if gene_id not in gene_variants:
+                        gene_variants[gene_id] = []
+                    gene_variants[gene_id].append({'pos': pos, 'ref': ref, 'alt': alt})
+    return gene_variants
+    
