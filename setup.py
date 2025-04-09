fc25640f168adb06e9728529f4b5005c103295dc
lyucheng <lyucheng@umich.edu>
2025-04-09 01:12:16 -0400

    include data and scripts for distribution

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ setup(
     author="Yu-Cheng Lin",
     description="A pipeline for GWAS analysis using phylogeny and gene mutations.",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        # Include all files in the data folder and optionally the scripts if needed
+        "MugGWAS": ["data/*", "design_documents/*","scripts/*", "tutorials/*"],
+    },
     install_requires=[
-        "pandas",
+        "pandas==2.1.4",
         "dendropy",
-        "gffutils",
+        "biopython==1.81",
+        "gffutils==0.13",
+        "argh",
         "setuptools"
     ],
 )
